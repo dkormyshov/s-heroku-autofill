76e5018caae796ab7e861aae700ba4ae40dcf024
Dmitry Kormyshov <dmitri.kormychov@360learning.com>
2023-08-17 17:46:01 +0300

    first

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const port = process.port || 3000
+
+app.use(express.json({extended: true, limit: '1mb'}))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// api endpoint
+app.post('/registration', (req, res) => {
+    const { username, email } = req.body
+    console.log(req.body)
+
+    // resend json data
+    res.status(200).json({ username, email })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+
+
+
+
