06a30451334d764dbfefa35beb334393dfcdbd9b
Dmitry Kormyshov <dmitri.kormychov@360learning.com>
2023-08-17 18:02:14 +0300

    heroku

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.port || 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json({extended: true, limit: '1mb'}))
 
